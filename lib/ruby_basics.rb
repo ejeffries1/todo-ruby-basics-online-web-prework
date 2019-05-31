9985d75843ef333b8a77673e694257b6fc48053e
Evan Jeffries <ejeffries14@gmail.com>
2019-05-31 05:56:51 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+ puts "#{num1}/#{num2}"
 end
 
 def assign_variable(value)
-  
+  puts "#{value}" = name
 end
 
-def argue
-  
+def argue(x)
+  puts "#{x}"
 end
 
-def greeting
-
+def greeting(greeting, name)
+return "#{greeting}"
 end
 
-def return_a_value
-
+def return_a_value(phrase == "Nice")
+puts "#{phrase}"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase="expert")
+ return "#{phrase}"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+return "#{topping}"
 end
